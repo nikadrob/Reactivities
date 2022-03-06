5d4784aeda68204fd43a68865559964979242e82
snasteski <nikadrob@live.com>
2022-03-06 16:02:13 +0100

    Initial front-end

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Header, List, ListItem } from 'semantic-ui-react';
+import axios from 'axios';
 
 function App() {
+  const [activities, setActivites] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then((response) => {
+      console.log(response);
+      setActivites(response.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header as="h2" icon="users" content="Reactivities" />
+
+      <List>
+        {activities.map((a: any) => (
+          <ListItem key={a.id}>{a.title}</ListItem>
+        ))}
+      </List>
     </div>
   );
 }
