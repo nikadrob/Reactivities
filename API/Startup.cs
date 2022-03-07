8ad0d81f74a0f1049928a57f9bbca4979f9268d0
snasteski <nikadrob@live.com>
2022-03-07 19:21:33 +0100

    Auto Mapper

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,7 @@
+using API.Extensions;
 using Application.Activities;
+using Application.Core;
+using AutoMapper;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
@@ -21,25 +24,8 @@ namespace API
     {
 
       services.AddControllers();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-      });
-
-      services.AddDbContext<DataContext>(opt =>
-      {
-        opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-      });
-
-      services.AddCors(opt =>
-      {
-        opt.AddPolicy("CorsPolicy", policy =>
-        {
-          policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-        });
-      });
-
-      services.AddMediatR(typeof(List.Handler).Assembly);
+      
+      services.AddApplicationServices(_config);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
